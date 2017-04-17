f08154fbae10c0c06f718838b175be8f0a90fda5
Tony Nguyen <tntuyen@ucdavis.edu>
2017-04-17 13:44:16 -0700

    heroku commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.send("Welcome")
+});
+
+var port = process.env.PORT || 3000
+app.listen(port, process.env.IP, function() {
+	console.log("Listening on port 3000");
+});
+
