dfaecca4000c2ef54f21a1242f7f862f1cf435bc
Tony Nguyen <tntuyen@ucdavis.edu>
2017-04-17 16:00:03 -0700

    add landing page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-	res.send("Welcome")
+	res.render("home")
 });
 
 var port = process.env.PORT || 3000
