82f240ee62bc314da745e909e361a7ec0f467ac4
Tony Nguyen <tntuyen@ucdavis.edu>
2017-04-17 17:17:27 -0700

    add script for email form send

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
 	res.render("home")
